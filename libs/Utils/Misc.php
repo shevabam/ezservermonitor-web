75e5432d53adec0340e37e800c8cc745bb172544
Michaël Marinetti <github@marinetti.fr>
2016-12-19 11:29:41 +0100

    Add methods in Util/Misc (exec/shellexec/cache/ago)
        Misc::cache($name, $data = null, $lifetime = 0) is a simple cache filesystem
        which will serialize $data to a file in write operation, or return
        the values in read operation. Writes are made if the argument
        `--save` is used when running the command.
    
        Misc::exec() and Misc::shellexec() will first check if
        a result is in cache for that command, and return it if exists.
        Otherwise, command will be executed then eventually saved in cache
        for next call (if `--save` argument has been used).

--- libs/Utils/Misc.php
+++ libs/Utils/Misc.php
@@ -2,6 +2,7 @@
 
 class Misc
 {
+    public static $cache = true;
     /**
      * Returns human size
      *
@@ -207,4 +208,161 @@ class Misc
 
         return false;
     }
+
+    public static function exec($command, &$output, &$return_var = null)
+    {
+        $config = Config::instance();
+        $output = Misc::cache($command);
+        //echo "on passe par Misc::exec";
+        if ($output)
+        {
+            //echo "found in cache";
+            return $output;
+            // exec
+        }
+        elseif ($config->get('esm:mode') == 'cron' && PHP_SAPI != 'cli')
+        {
+            //echo "mode CRON absent\n";
+            return false;
+        }
+        else
+        {
+            //echo "On EXECUTE\n";
+            if ($return_var) {
+                $return = exec($command, $output, $return_var);
+                if ($return_var != 0)
+                {
+                    //echo "\n\tERROR with $command\n\n";
+                    header('HTTP/1.0 500 Server Error');
+                    return false;
+                }
+            }
+            else {
+                $return = exec($command, $output);
+            }
+
+            if (isset($_SERVER['argv']) && in_array('--save', $_SERVER['argv']))
+            {
+                $written = Misc::cache($command, $output);
+            }
+        }
+        return $return;
+    }
+
+    public static function shellexec($command, $args = null)
+    {
+        $config = Config::instance();
+        $output = Misc::cache($command.$args);
+        if ($output)
+        {
+            //echo "$command.$args found in cache<br>";
+            //var_dump($output);
+            return $output;
+        }
+        elseif ($config->get('esm:mode') == 'cron' && PHP_SAPI != 'cli')
+        {
+            return null;
+        }
+        else
+        {
+            $output = shell_exec($command.$args);
+            if (is_array($output))
+            {
+                die("wtf output of shell_exec is an array ???");
+            }
+            else
+            {
+                //echo "output is a string: $output";
+            }
+            if (isset($_SERVER['argv']) && in_array('--save', $_SERVER['argv']))
+                Misc::cache($command.$args, $output);
+        }
+        return $output;
+    }
+
+    static public function cache($name, $data = null, $lifetime = 0)
+    {
+        if (empty($name))
+            throw new Exception("invalid cache key");
+        $config = Config::instance();
+        $salt   = $config->get('esm:salt');
+        $file = sha1($name.$salt).'.txt';
+        //echo "$name \n<br>\t$file\n<br>";
+        $dir = ROOTPATH.DIRECTORY_SEPARATOR.$config->get('esm:cron_output_path').DIRECTORY_SEPARATOR;
+        if ($data === NULL)
+        {
+
+            if (isset($_SERVER['argv']) && in_array('--save', $_SERVER['argv']))
+            {
+                //echo "<br>\n\nCACHE REMOVED\n\n<br>";
+                Misc::$cache = false;
+                return false;
+            }
+
+
+            if (is_file($dir.$file))
+            {
+                if (!Misc::$cache || !$lifetime || ((time() - filemtime($dir.$file)) < $lifetime))
+                {
+                    try
+                    {
+                        return unserialize(file_get_contents($dir.$file));
+                    }
+                    catch(Exception $e)
+                    {
+                    }
+                }
+                else
+                {
+                    try{
+                        unlink($dir.$file);
+                    }
+                    catch(Exception $e)
+                    {
+                    }
+                }
+            }
+            else
+            {
+                //echo "file not exists: <b>$name ($file)</b> \n<br>";
+            }
+            //echo "nothing found in cache for $name\n";
+            return NULL;
+        }
+        $data = serialize($data);
+        try
+        {
+            $written = file_put_contents($dir.$file, $data, LOCK_EX);
+            //echo "\nWRITE for $name \n\tIN $file:\n\t\t$data\nENDOFWRITE";
+            return (bool)$written;
+        }
+        catch(Exception $e)
+        {
+            return false;
+        }
+    }
+
+    function ago($time)
+    {
+        $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+        $lengths = array("60","60","24","7","4.35","12","10");
+
+        $now = time();
+
+        $difference     = $now - $time;
+        $tense         = "ago";
+
+        for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+            $difference /= $lengths[$j];
+        }
+
+        $difference = round($difference);
+
+        if($difference != 1) {
+            $periods[$j].= "s";
+        }
+
+        return "$difference $periods[$j] 'ago' ";
+    }
 }
+
