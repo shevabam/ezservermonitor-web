4e8eb68ed7bf2c64175d3a7315d8e3ee17479801
ShevAbam <shevabam@gmail.com>
2018-05-23 15:40:01 +0200

    Fix sequence of CPU load from mobile #57

--- index.php
+++ index.php
@@ -129,15 +129,17 @@ $update = $Config->checkUpdate();
                 <input type="text" class="gauge" id="load-average_1" value="0" data-height="100" data-width="150" data-min="0" data-max="100" data-readOnly="true" data-fgColor="#BED7EB" data-angleOffset="-90" data-angleArc="180">
             </div>
 
-            <div class="f-right w33p">
+            <div class="f-left w33p">
+                <h3>5 min</h3>
+                <input type="text" class="gauge" id="load-average_5" value="0" data-height="100" data-width="150" data-min="0" data-max="100" data-readOnly="true" data-fgColor="#BED7EB" data-angleOffset="-90" data-angleArc="180">
+            </div>
+
+            <div class="f-left w33p">
                 <h3>15 min</h3>
                 <input type="text" class="gauge" id="load-average_15" value="0" data-height="100" data-width="150" data-min="0" data-max="100" data-readOnly="true" data-fgColor="#BED7EB" data-angleOffset="-90" data-angleArc="180">
             </div>
 
-            <div class="t-center">
-                <h3>5 min</h3>
-                <input type="text" class="gauge" id="load-average_5" value="0" data-height="100" data-width="150" data-min="0" data-max="100" data-readOnly="true" data-fgColor="#BED7EB" data-angleOffset="-90" data-angleArc="180">
-            </div>
+            <div class="cls"></div>
         </div>
     </div>
 
