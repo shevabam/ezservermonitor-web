bd487ef4cf59d227083081ba814121cd76600873
Mark Schlögel <44850130+MarkSchloegel@users.noreply.github.com>
2020-06-10 21:31:35 +0200

    show raspberry pi vcgenmod status

--- libs/pistatus.php
+++ libs/pistatus.php
@@ -22,29 +22,29 @@ if ($Config->get('pistatus:enable'))
     
     $vcgencmd_output = hexdec(trim(shell_exec('vcgencmd get_throttled'), 'throttled='));
     
-    if ($vcgencmd_output & Under_Voltage_Detected) { $datas[] = array( 'name' => "Under voltage detected" , 'status' => 0 ); }
-    else { $datas[] = array( 'name' => "Under Voltage detected" , 'status' => 1 ); }
+    if ($vcgencmd_output & Under_Voltage_Detected) { $datas[] = array( 'name' => "Under voltage detected" , 'status' => 1 ); }
+    else { $datas[] = array( 'name' => "Under Voltage detected" , 'status' => 0 ); }
     
-    if ($vcgencmd_output & ARM_Frequency_Capped) { $datas[] = array( 'name' => "ARM frequency capped" , 'status' => 0 ); }
-    else { $datas[] = array( 'name' => "ARM frequency capped" , 'status' => 1 ); }
+    if ($vcgencmd_output & ARM_Frequency_Capped) { $datas[] = array( 'name' => "ARM frequency capped" , 'status' => 1 ); }
+    else { $datas[] = array( 'name' => "ARM frequency capped" , 'status' => 0 ); }
     
-    if ($vcgencmd_output & Currently_Throttled) { $datas[] = array( 'name' => "Currently throttled" , 'status' => 0 ); }
-    else { $datas[] = array( 'name' => "Currently throttled" , 'status' => 1 ); }
+    if ($vcgencmd_output & Currently_Throttled) { $datas[] = array( 'name' => "Currently throttled" , 'status' => 1 ); }
+    else { $datas[] = array( 'name' => "Currently throttled" , 'status' => 0 ); }
     
-    if ($vcgencmd_output & Soft_Temperature_Limit_Active) { $datas[] = array( 'name' => "Soft temperature limit active" , 'status' => 0 ); }
-    else { $datas[] = array( 'name' => "Soft temperature limit active" , 'status' => 1 ); }
+    if ($vcgencmd_output & Soft_Temperature_Limit_Active) { $datas[] = array( 'name' => "Soft temperature limit active" , 'status' => 1 ); }
+    else { $datas[] = array( 'name' => "Soft temperature limit active" , 'status' => 0 ); }
     
-    if ($vcgencmd_output & Under_Voltage_Has_Occurred) { $datas[] = array( 'name' => "Under voltage has occurred" , 'status' => 0 ); }
-    else { $datas[] = array( 'name' => "Under voltage has occurred" , 'status' => 1 ); }
+    if ($vcgencmd_output & Under_Voltage_Has_Occurred) { $datas[] = array( 'name' => "Under voltage has occurred" , 'status' => 1 ); }
+    else { $datas[] = array( 'name' => "Under voltage has occurred" , 'status' => 0 ); }
     
-    if ($vcgencmd_output & ARM_Frequency_Capping_Has_Occurred) { $datas[] = array( 'name' => "ARM frequency capping has occurred" , 'status' => 0 ); }
-    else { $datas[] = array( 'name' => "ARM frequency capping has occurred" , 'status' => 1 ); }
+    if ($vcgencmd_output & ARM_Frequency_Capping_Has_Occurred) { $datas[] = array( 'name' => "ARM frequency capping has occurred" , 'status' => 1 ); }
+    else { $datas[] = array( 'name' => "ARM frequency capping has occurred" , 'status' => 0 ); }
     
-    if ($vcgencmd_output & Throttling_Has_Occurred) { $datas[] = array( 'name' => "Throttling has occurred" , 'status' => 0 ); }
-    else { $datas[] = array( 'name' => "Throttling has occurred" , 'status' => 1 ); }
+    if ($vcgencmd_output & Throttling_Has_Occurred) { $datas[] = array( 'name' => "Throttling has occurred" , 'status' => 1 ); }
+    else { $datas[] = array( 'name' => "Throttling has occurred" , 'status' => 0 ); }
     
-    if ($vcgencmd_output & Soft_Temperature_Limit_Has_Occurred) { $datas[] = array( 'name' => "Soft temperature limit has occurred" , 'status' => 0 ); }
-    else { $datas[] = array( 'name' => "Soft temperature limit has occurred" , 'status' => 1 ); }
+    if ($vcgencmd_output & Soft_Temperature_Limit_Has_Occurred) { $datas[] = array( 'name' => "Soft temperature limit has occurred" , 'status' => 1 ); }
+    else { $datas[] = array( 'name' => "Soft temperature limit has occurred" , 'status' => 0 ); }
 }
 
 echo json_encode($datas);
